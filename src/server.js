f541a146e561768c4d0f5f2107054fc1ea0b7469
sahilsharma746 <sharmasahil00746@gmail.com>
2026-04-29 13:29:25 +0530

    Remove View Recent Logs button from dashboard

--- src/server.js
+++ src/server.js
@@ -393,10 +393,6 @@ app.get('/', (req, res) => {
           <svg viewBox="0 0 24 24"><path d="M22 12h-4l-3 9L9 3l-3 9H2"/></svg>
           Health Check
         </a>
-        <a class="link-btn" href="/api/logs" target="_blank" rel="noopener">
-          <svg viewBox="0 0 24 24"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><polyline points="14 2 14 8 20 8"/><line x1="16" y1="13" x2="8" y2="13"/><line x1="16" y1="17" x2="8" y2="17"/><polyline points="10 9 9 9 8 9"/></svg>
-          View Recent Logs
-        </a>
       </div>
     </div>
   </div>
